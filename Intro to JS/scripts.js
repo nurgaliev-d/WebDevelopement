b1e0731bee08ffd7b990bb6f9cf92aeab3aa1c40
nurgaliev-d <nurgalyev.dias@gmail.com>
2026-07-14 17:12:13 +0500

    intro to js completed

---
+++ Intro to JS/scripts.js
@@ -0,0 +1,4 @@
+console.log("Hello, World!");
+// func console.log displays a message in the browser's console
+alert("Welcome to JavaScript!");
+// func alert shows a pop-up message to the user with the text "Welcome to JavaScript!"
