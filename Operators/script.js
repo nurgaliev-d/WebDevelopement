69943c20611b8f1f5b80a64cd18583512380d0ec
nurgaliev-d <nurgalyev.dias@gmail.com>
2026-07-19 19:19:15 +0500

    operations completed

---
+++ Operators/script.js
@@ -0,0 +1,40 @@
+//1-task
+let a = 10
+let b = 5
+let c = 2
+
+console.log(a + b + c)
+console.log(a - b + c)
+console.log((a + b) / c)
+console.log(a % b)
+
+// //2-task
+// let x = 7
+// console.log(x++ + ++x)
+// console.log(x-- - --x)
+
+//3-task
+let d = 15
+let e = 16
+let resultString = "The result is: "
+let sumString = String(d + e)
+
+console.log(resultString + sumString)
+
+//4-task
+console.log(a > b && a > 0 && b > 0)
+console.log(c < 10 || c == 2)
+console.log( a / c !=5)
+
+//5-task
+
+console.log(10 == '10')
+console.log(10 === '10')
+// '==' checks only values, '===' checks both values and data types
+
+//6-task
+let x = 10
+let y = 4
+console.log('Task 6')
+console.log((x % 2 == 0 || x % 3 == 0) && x % 6 != 0)
+console.log(!(y > 10 || y < 5))
