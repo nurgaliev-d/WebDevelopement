fbbd9758d8503feb1e18f7d9df5614e97c368214
Nurgaliev Dias <123064954+nurgaliev-d@users.noreply.github.com>
2026-07-24 19:33:21 +0500

    Loops completed

---
+++ Loops/script.js
@@ -0,0 +1,93 @@
+//Task1
+let counter = 1
+while (counter <= 10) {
+    console.log(counter)
+    counter++
+}
+
+// //Task2
+// let counter2 = 1
+// while (counter2 <= 10) {
+//     if (counter2 == 5) {
+//         counter2++
+//         continue
+//     }
+//     console.log(counter2)
+//     counter2++
+// }
+
+// //Task3
+// let counter3 = 1
+
+// do {
+//     console.log(counter3)
+//     counter3++
+// } while (counter3 <= 5)
+// //do while runs code ate least 1 time, bc it runs do block before checking while block.
+
+// //Task4
+// let input
+
+// while (true) {
+//     input = prompt('Input the word!')
+//     if (input == 10) {
+//         console.log('Stopped')
+//         break
+//     }
+// }
+
+// //Task5
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i)
+// }
+
+// //Task6
+// let y = 0
+// for(;;) {
+//     y++
+//     console.log(y)
+//     if (y ==100) {
+//         console.log('variable y is 100, loop stopped')
+//         break
+//     }
+// }
+
+// //Task7
+// let numbers = [1, 2, 3, 4, 5]
+
+// for (let i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i])
+// }
+
+// //Task8
+// // We use FOR loop when we know length of the array, while loop when the program depends on the input value.
+
+// const colors = ['red', 'green', 'blue'];
+
+// for (let i = 0; i < colors.length; i++) {
+//   console.log(colors[i]);
+// }
+
+
+// let number = 0;
+
+// // Цикл крутится, пока число меньше 0.5
+// while (number < 0.5) {
+//   number = Math.random();
+//   console.log('Выпало число:', number);
+// }
+
+// //Task9
+// for (let i = numbers.length - 1; i >= 0; i--) {
+//     console.log(numbers[i])
+// }
+
+// //Task10
+// let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] % 2 !== 0) {
+//         continue
+//     }
+//     console.log(nums[i])
+// }
